cc0163c51953ff5d5ea05c117eb7d132e225b984
Marcel <marcelpodsiadlo.business@gmail.com>
2023-10-07 16:52:02 +0200

    Update files

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './components/css/index.css'
 import { BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 
-import {Home, Login, StartGame} from './components/game/main.jsx'
+import {Home, Login, SignUp , StartGame} from './components/game/main.jsx'
 
 export default function App() {
   return (
@@ -10,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/game_0" element={<StartGame />} />
       </Routes>
     </Router>
--- game/src/components/game/main.jsx
+++ game/src/components/game/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-
+import React, {useState} from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import '../css/main.css'
 
 export function Home() {
   return (
@@ -10,17 +10,90 @@ export function Home() {
       <Link to="/login"><button className="buttons">Zaloguj się</button></Link>
       <Link to="/game_0"><button className="buttons">Rozpocznij grę</button></Link>
       </div>
+
+      <div id="money"> Twoje saldo: </div>
     </div>
   )
 }
 
-export function Login() {
-  return (
-  <div className="container flex_center">
-    <h1>Login</h1>
+// Login and SignUp components
 
-  </div>
-  )
+export function Login() {
+
+  const navigate = useNavigate()
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(null);
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userName, password }),
+      });
+
+      if (response.ok) {
+        navigate('./game_0')
+      } else {
+        setLoginError("Niepoprawne dane logowania.");
+      }
+    } catch (error) {
+      console.error(error);
+      setLoginError("Wystąpił błąd podczas logowania.");
+    }
+  };
+
+  return (
+    <div className="container flex_center">
+      <h1>Logowanie</h1>
+      <form className="forms_login_signup flex_center" onSubmit={handleLoginSubmit}>
+        <label htmlFor="user_name-login">Nazwa Użytkownika:</label>
+        <input
+          type="text"
+          id="user_name-login"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <label htmlFor="password-login">Hasło:</label>
+        <input
+          type="password"
+          id="password-login"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Zaloguj" className="submit_button" />
+      </form>
+      {loginError && <p>{loginError}</p>}
+      <Link to="/signup"><button className="button_go">Nie masz jeszcze konta? <strong>Zarejestruj się</strong></button></Link>
+      <Link to="/">Powrót na stronę główną</Link>
+    </div>
+  );
+}
+
+export function SignUp() {
+  return (
+    <div className="container flex_center">
+      <h1>Rejestracja</h1>
+      <form className="forms_login_signup flex_center" id="login_form">
+        <label for="user_name-signup">Nazwa Użytkownika: </label>
+        <input 
+        type="text" 
+        id="user_name-signup" />
+        <label for="password-signup">Hasło: </label>
+        <input 
+        type="password" 
+        id="password-signup" />
+        <input value="Zarejestruj sie" type="submit" id="submit-signup" className="submit_button"  />
+      </form>
+      <Link to="/login"><button id="button-go-login" className="button_go">Masz już konto? <strong>Zaloguj się</strong></button></Link>
+      <Link to="/">Powrót na stronę główną</Link>
+    </div>
+    )
 }
 
 export function StartGame() {
