5696876e13e68665bcf0741f3ee4b50fc1bb5b4b
Marcel <marcelpodsiadlo.business@gmail.com>
2023-10-07 15:19:38 +0200

    Add link to login page and game board

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './components/css/index.css'
 import { BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 
-import {Home} from './components/game/main.jsx'
+import {Home, Login, StartGame} from './components/game/main.jsx'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/game_0" element={<StartGame />} />
       </Routes>
     </Router>
   )
--- game/src/components/game/main.jsx
+++ game/src/components/game/main.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
 
 export function Home() {
   return (
-  <h1>Home</h1>
+    <div className="container flex_center">
+      <h1>Witaj</h1>
+      <div className="buttons_box">
+      <Link to="/login"><button className="buttons">Zaloguj się</button></Link>
+      <Link to="/game_0"><button className="buttons">Rozpocznij grę</button></Link>
+      </div>
+    </div>
+  )
+}
+
+export function Login() {
+  return (
+  <div className="container flex_center">
+    <h1>Login</h1>
+
+  </div>
+  )
+}
+
+export function StartGame() {
+  return (
+  <div className="container flex_center">
+    <h1>Start Game</h1>
+
+  </div>
   )
 }
